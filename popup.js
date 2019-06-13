9a63853795568a0667ce4807f9ef4b3eb65c3192
Jakob Kousholt <jake@Jakobs-MacBook-Pro.local>
2019-06-12 23:28:42 -0400

    we got caching

--- popup.js
+++ popup.js
@@ -7,26 +7,43 @@
 // }
 
 // document.getElementById('changeColor').addEventListener('click', hello);
+let cache = {};
+// let test = chrome.storage.sync.get(['BFO'], function(result) {
+//   // console.log('Value currently is ' + result.url);
+//     if (result === undefined) {
+//       cache = ['a'];
+//     } else {
+//       cache = ['b'];
+//     }
+//     return 1;
 
+// });
 
-
-
+// let smp = document.createElement('h1');
+// smp.innerText = JSON.stringify(test);
+// document.querySelector('body').appendChild(smp);
 
 chrome.tabs.query({currentWindow: true}, function(tabs) {
+  let key = Math.random();
+  const wrapper = {'BFO': cache};
+  wrapper['BFO'][key] = [];
   tabs.forEach(tab => {
     let li = document.createElement('li');
-    chrome.storage.sync.set(tab, function() {
-      console.log('Value is set to ' + tab.url);
-    });
-    chrome.storage.sync.get(['url'], function(result) {
-      // console.log('Value currently is ' + result.url);
-      li.innerHTML = result.url;
-    });
+    li.innerText = tab.url;
     document.querySelector('ul').appendChild(li);
-
+    wrapper['BFO'][key].push(tab.url);
+    chrome.storage.sync.set(wrapper, function() {
+    });
   });
 });
 
+let tester = document.createElement('h1');
+
+chrome.storage.sync.get(['BFO'], function(result) {
+  // console.log('Value currently is ' + result.url);
+  tester.innerText = JSON.stringify(result['BFO']);
+  document.querySelector('body').appendChild(tester);
+});
 
 // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 //   document.querySelector('li').innerHTML = tabs[0].url;
